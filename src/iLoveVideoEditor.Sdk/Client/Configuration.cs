b9444cf58d643c977a345f8cc53a4625efe8ea92
avasile <username@jwplayer.com>
2026-07-16 06:11:51 +0300

    chore(release): 1.0.1

--- src/iLoveVideoEditor.Sdk/Client/Configuration.cs
+++ src/iLoveVideoEditor.Sdk/Client/Configuration.cs
@@ -33,7 +33,7 @@ namespace iLoveVideoEditor.Sdk.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1";
 
         /// <summary>
         /// Identifier for ISO 8601 DateTime Format
@@ -117,7 +117,7 @@ namespace iLoveVideoEditor.Sdk.Client
         public Configuration()
         {
             Proxy = null;
-            UserAgent = WebUtility.UrlEncode("OpenAPI-Generator/1.0.0/csharp");
+            UserAgent = WebUtility.UrlEncode("OpenAPI-Generator/1.0.1/csharp");
             BasePath = "https://api.ilovevideoeditor.com";
             DefaultHeaders = new ConcurrentDictionary<string, string>();
             ApiKey = new ConcurrentDictionary<string, string>();
@@ -540,7 +540,7 @@ namespace iLoveVideoEditor.Sdk.Client
             report += "    OS: " + System.Environment.OSVersion + "\n";
             report += "    .NET Framework Version: " + System.Environment.Version  + "\n";
             report += "    Version of the API: 1.0.0\n";
-            report += "    SDK Package Version: 1.0.0\n";
+            report += "    SDK Package Version: 1.0.1\n";
 
             return report;
         }
